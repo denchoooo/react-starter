e5d69eef6457f7c8a05e95b3766c7c09e5fe7603
Deni Cho <dencho.12@gmail.com>
2017-07-15 23:46:44 +0800

    Edited home page and file structure

---
+++ src/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>React Starter</h1>
+        </div>
+        <div className="App-footer">
+          Made by <a href="https://twitter.com/denichodev">Deni Cho</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
